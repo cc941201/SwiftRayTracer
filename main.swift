c2bc339deb6693236dcc9d7e9720646ba89ad966
张国晔 <cc941201@gmail.com>
2015-06-07 19:40:16 +0800

    Optimization on Normal Computation

--- main.swift
+++ main.swift
@@ -63,18 +63,37 @@ struct Ray {
 }
 
 struct Face {
-    let v0, v1, v2, e0, e1, e2, n0, n1, n2: Vector
+    let v0, e0, e1, n0, nd0, nd1: Vector
     
     init(v0: Vector, v1: Vector, v2: Vector, n0: Vector, n1: Vector, n2: Vector) {
-        self.v0 = v0
-        self.v1 = v1
-        self.v2 = v2
-        e0 = v1 - v0
-        e1 = v2 - v0
-        e2 = v2 - v1
-        self.n0 = n0
-        self.n1 = n1
-        self.n2 = n2
+        let e0 = v1 - v0, e1 = v2 - v0, e2 = v2 - v1
+        let sn1, sn2: Vector
+        if abs(e0.x) > abs(e1.x) || abs(e0.x) > abs(e2.x) {
+            if abs(e1.x) > abs(e2.x) {
+                self.e0 = e0
+                self.e1 = e1
+                self.v0 = v0
+                self.n0 = n0
+                sn1 = n1
+                sn2 = n2
+            } else {
+                self.e0 = e2
+                self.e1 = -e0
+                self.v0 = v1
+                self.n0 = n1
+                sn1 = n2
+                sn2 = n0
+            }
+        } else {
+            self.e0 = -e1
+            self.e1 = -e2
+            self.v0 = v2
+            self.n0 = n2
+            sn1 = n0
+            sn2 = n1
+        }
+        nd0 = sn1 - self.n0
+        nd1 = sn2 - self.n0
     }
     
     func intersect(ray: Ray) -> Float? {
@@ -99,35 +118,13 @@ struct Face {
     }
     
     func normal(point: Vector) -> Vector {
-        let p1, p2, normal1, normal2: Vector
-        if e0.x > e1.x || e0.x > e2.x {
-            let ratio = (point.x - v0.x) / e0.x
-            p1 = e0 * ratio + v0
-            normal1 = (n1 - n0) * ratio + n0
-            if e1.x > e2.x {
-                let ratio = (point.x - v0.x) / e1.x
-                p2 = e1 * ratio + v0
-                normal2 = (n2 - n0) * ratio + n0
-            } else {
-                let ratio = (point.x - v1.x) / e2.x
-                p2 = e2 * ratio + v1
-                normal2 = (n2 - n1) * ratio + n1
-            }
-        } else {
-            let ratio1 = (point.x - v0.x) / e1.x
-            p1 = e1 * ratio1 + v0
-            normal1 = (n2 - n0) * ratio1 + n0
-            let ratio2 = (point.x - v1.x) / e2.x
-            p2 = e2 * ratio2 + v1
-            normal2 = (n2 - n1) * ratio2 + n1
-        }
-        let ratio: Float
-        let ydiff = p2.y - p1.y, zdiff = p2.z - p1.z
-        if abs(ydiff) > abs(zdiff) {
-            ratio = (point.y - p1.y) / ydiff
-        } else {
-            ratio = (point.z - p1.z) / zdiff
-        }
+        let ratio1 = (point.x - v0.x) / e0.x
+        let p1 = e0.y * ratio1 + v0.y
+        let normal1 = nd0 * ratio1 + n0
+        let ratio2 = (point.x - v0.x) / e1.x
+        let p2 = e1.y * ratio2 + v0.y
+        let normal2 = nd1 * ratio2 + n0
+        let ratio = (point.y - p1) / (p2 - p1)
         return (normal2 - normal1) * ratio + normal1
     }
 }
